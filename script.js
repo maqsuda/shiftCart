54fd86af99ec091771f3dcace84e765207323c27
Maqsuda Binte Mahboob <maqsudamahboob@gmail.com>
2026-02-17 20:19:13 +0600

    java script

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("lets start");
+const loadCategory = () => {
+  fetch("https://fakestoreapi.com/products/categories")
+    .then((res) => res.json())
+    .then((data) => displayCategory(data));
+};
+const displayCategory = (data) => {
+  console.log(data);
+};
+
+loadCategory();
