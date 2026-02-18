404a5bea251189af8e4d4af9dcafa955d707213c
Maqsuda Binte Mahboob <maqsudamahboob@gmail.com>
2026-02-18 23:12:14 +0600

    product modal add cart

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ const loadProduct = () => {
     .then((data) => displayProduct(data));
 };
 
+const loadAllProduct = () => {
+  fetch("https://fakestoreapi.com/products")
+    .then((res) => res.json())
+    .then((data) => displayAllCategory(data));
+};
+
 const loadCategory = () => {
   fetch("https://fakestoreapi.com/products/categories")
     .then((res) => res.json())
@@ -16,7 +22,12 @@ const loadProductCategoryWise = () => {
     .then((data) => displayCategory(data));
 };
 
-
+const addCount = () => {
+  const cart = document.getElementById("countCart").innerText;
+  console.log(cart);
+  let count = parseInt(cart) + 1;
+  document.getElementById("countCart").innerText = count;
+};
 
 const displayCategory = (categories) => {
   for (let category of categories) {
@@ -25,7 +36,7 @@ const displayCategory = (categories) => {
     btnDiv.innerHTML = "";
     btnDiv.innerHTML = `
      <div class="mb-5">
-     <button class="btn btn-outline rounded-full">${category}</button>
+       <button class="btn btn-outline rounded-full">${category}</button>
      </div>
     `;
 
@@ -81,8 +92,33 @@ const hideAndShow = () => {
     .classList.add("hidden");
 };
 
-const displayAllCategory = () => {
+const homebtn = () => {
+  const bannerContainer = document
+    .getElementById("banner-container")
+    .classList.remove("hidden");
+
+  const chooseContainer = document
+    .getElementById("choose-container")
+    .classList.remove("hidden");
+
+  const trendingContainer = document
+    .getElementById("trending-container")
+    .classList.remove("hidden");
+
+  const allProductContainer = document
+    .getElementById("allProduct-container")
+    .classList.add("hidden");
+
+  const btnHome = document.getElementById("btn-home");
+  btnHome.classList.add("text-blue-500", "font-bold");
+
+  const btnProduct = document.getElementById("btn-product");
+  btnProduct.classList.remove("text-blue-500", "font-bold");
+};
+
+const displayAllCategory = (products) => {
   hideAndShow();
+
   const btnHome = document.getElementById("btn-home");
   btnHome.classList.remove("text-blue-500", "font-bold");
 
@@ -90,11 +126,42 @@ const displayAllCategory = () => {
   btnProduct.classList.add("text-blue-500", "font-bold");
 
   const allProductContainer = document.getElementById("allProduct-container");
-  allProductContainer.removeAttribute("hidden");
-  // .removeAttribute("hidden");
+  allProductContainer.classList.remove("hidden");
 
-  loadCategory();
+  const productContainer = document.getElementById("product-container");
+  productContainer.innerHTML = "";
+  for (let product of products) {
+    const btnDiv = document.createElement("div");
+    btnDiv.classList.add("card", "bg-base-100", "shadow-sm", "mb-5", "h-full");
+    btnDiv.innerHTML = `
+    
+                    <figure class="bg-gray-100 justify-center items-center">
+                        <img class="h-[250px] w-[250px] px-5 items-center" src=${product.image}
+                            alt="Shoes" />
+                    </figure>
+                    <div class="card-body">
+                        <div class="card-title flex justify-between ">
+                          <div class="text-blue-700 bg-blue-50 rounded-full py-1 px-2"> ${product.category}</div>
+
+                            <div class=""><i class="fa-solid fa-star text-yellow-500 mr-2"></i>${product.rating.rate}(${product.rating.count})</div>
+                        </div>
+                        <p>${product.title}
+                        </p>
+                        <p class="font-bold">$${product.price}</p>
+                        <div class="flex justify-between items-center gap-5">
+                            <div onClick="my_modal_5.showModal()" class="btn btn-outline hover:bg-blue-700 hover:text-white px-8"><i class="fa-regular fa-eye"></i>Details</div>
+                            <div onClick="addCount()" class="btn btn-outline hover:bg-blue-700 hover:text-white px-8"><i class="fa-solid fa-cart-plus"></i>Add</div>
+                        </div>
+                    </div>
+               
+
+
+                
+`;
+    productContainer.append(btnDiv);
+  }
 };
 
 loadProduct();
-// loadCategory();
+loadCategory();
+loadAllProduct();
